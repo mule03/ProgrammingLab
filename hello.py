0b8838ef24d2ac9e68ba8e82c6848e11b12bdc64
Il Tuo Nome <anto.mule2003@gmail.com>
2024-11-13 10:32:25 +0100

    bb

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
 print("hello pyjf")
+ciao=2
+ciao2=3
+print('Valore 1: {}, valore 2: {B}'.format(ciao, B=5))
+if (ciao2 > ciao):(
+print("My var is bigger than yours"))
+if (ciao2-ciao) <= 1:(
+print("...but not so much") )
