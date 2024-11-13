e462754912580070fd87d1961ce8f5aef6a42224
Il Tuo Nome <anto.mule2003@gmail.com>
2024-11-13 10:55:49 +0100

    ssss

--- hello.py
+++ hello.py
@@ -6,3 +6,25 @@ if (ciao2 > ciao):
     print("My var is bigger than yours")
     if (ciao2-ciao) <= 1:(
         print("...but not so much") )
+a=1
+b=2
+def somma(a, b):
+    """
+    Calcola la somma di due numeri.
+    Args:
+    a (int or float): Il primo numero.
+    b (int or float): Il secondo numero.
+    Returns:
+    int or float: La somma dei due numeri.
+    """
+    return a + b
+
+risultato=somma(a,b)
+print('Valore 1: {}'.format(risultato))
+def ss(a,k):
+    """d
+    fff
+    """
+    return a*k
+
+
